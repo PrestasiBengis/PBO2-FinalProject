3c31274b3127e5fae6df8c164ea0327be9fe92b4
domarco <nnnmsgl@gmail.com>
2015-05-22 17:01:10 +0700

    Tugas Akhir PBO2 - Sistem SKKm
    Folder project

---
+++ FinalProject - Sistem SKKM/src/com/fantastic5/skkmprogram/DbConnector.java
@@ -0,0 +1,7 @@
+package com.fantastic5.skkmprogram;
+
+public class DbConnector {
+
+	static final String driver = "com.mysql.jdbc.Driver";
+	static final String dbUrl = "jdbc:mysql://localhost/fantastic5";
+}
